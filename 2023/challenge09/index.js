698c71152754e54c2d35c63d5a04a7f5b1ad15bf
Alex Martinez Jativa <alex.martinez.jativa@gmail.com>
2024-09-01 14:54:58 +0500

    add challenge09

---
+++ 2023/challenge09/index.js
@@ -0,0 +1,30 @@
+function adjustLights(lights) {
+
+  const evenValue = lights[lights.length -1]
+  const oddValue = lights.find((el) => el !== evenValue)
+  let count = 0
+
+  for (let i = 0; i < lights.length; i++) {
+    if (i === 0) {
+      if (lights[i] !== evenValue) {
+        count++
+      }
+    }
+    else if (i % 2 === 0) {
+      if (lights[i] !== evenValue) {
+        count ++
+      }
+    } else {
+      if (lights[i] !== oddValue) {
+        count ++
+      }
+    }
+  }
+  return count
+}
+
+const res = adjustLights(["🔴", "🔴", "🟢", "🔴", "🟢"])
+
+console.log(res)
+
+module.exports = adjustLights
